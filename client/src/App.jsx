8b18dfd1ce5c027c859a46061f11c68ee8e4ae9b
petar-arshinkov <143599367+petar-arshinkov@users.noreply.github.com>
2025-11-30 15:22:02 +0200

    Setup Tailwind CSS

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,19 @@
+import { Routes } from "react-router"
 
 function App() {
 
 
   return (
     <>
-        <h1>Hello, React!</h1>
+
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
+
+      <Routes>
+
+
+      </Routes>
     </>
   )
 }
